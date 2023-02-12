f3e57be2aab8e217d3ce7500efb064e458ad7dec
mcrombeen <43091597+mcrombeen@users.noreply.github.com>
2023-02-12 09:23:43 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+const button1 = document.getElementById("button1");
+const button2 = document.getElementById("button2");
+const button3 = document.getElementById("button3");
+const button4 = document.getElementById("button4");
+
+button1.addEventListener("click", function() {
+  window.open("https://docs.google.com/forms/d/form1-url");
+});
+
+button2.addEventListener("click", function() {
+  window.open("https://docs.google.com/forms/d/form2-url");
+});
+
+button3.addEventListener("click", function() {
+  window.open("https://docs.google.com/forms/d/form3-url");
+});
+
+button4.addEventListener("click", function() {
+  window.open("https://docs.google.com/forms/d/form4-url");
+});
